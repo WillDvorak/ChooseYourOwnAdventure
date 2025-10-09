ef77d1746e36f7131e1bed9bcdb308e9345a7187
Manohara Maraka <manoharamaraka@ManoharMac.local>
2025-10-09 17:34:25 -0400

    add Spring Boot backend (Gradle) under /api folder

---
+++ api/src/main/java/com/textquest/api/TextQuestApiApplication.java
@@ -0,0 +1,13 @@
+package com.textquest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextQuestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextQuestApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/textquest/api/TextQuestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.textquest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextQuestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
