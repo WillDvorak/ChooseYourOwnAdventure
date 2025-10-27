ce47944c23f8cb91902465bc2818298fa4db24a9
Zhiwei Song <zhiweis@cs506x10.cs.wisc.edu>
2025-10-27 15:41:47 -0500

    Modified frontend to run the new UI and run on CSL VM

--- ReactFrontend/ChooseYourOwnAdventure/src/main.jsx
+++ ReactFrontend/ChooseYourOwnAdventure/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Textbox from './components/Textbox.jsx'
+import AppLayout from './components/structural/AppLayout.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-    <Textbox />
+    <AppLayout />
     // To use the test skeleton instead:
     // <SimpleTest_test />
 )
