3038a48f5252797d1766d8f5b6876d4f3bf238d8
WillDvorak <wjdvorak@wisc.edu>
2025-11-19 19:41:23 -0600

    more edits

--- ReactFrontend/ChooseYourOwnAdventure/src/components/content/DisplayBox.jsx
+++ ReactFrontend/ChooseYourOwnAdventure/src/components/content/DisplayBox.jsx
@@ -1,8 +1,12 @@
 import { Card } from "react-bootstrap"
 
+/**
+ * 
+ * @param {jsObject} props.theme -> js object with theme attributes
+ * @param {*} props.sceneInfo -> js object with scene attributes 
+ * @returns 
+ */
 export default function DisplayBox(props) {
-
-
     return <Card
         style={{
             border: props.theme.containerBorder,
@@ -15,17 +19,12 @@ export default function DisplayBox(props) {
         {props.sceneInfo ? <>
             <h2>{props.sceneInfo.title}</h2>
             <h3>{props.sceneInfo.code}</h3>
-            <p>HARDCODE</p>
-            <p>HARDCODE</p>
-            <p>HARDCODE</p>
+            
         </>
         :
         <>
             <h2>HARDCODE</h2>
             <h3>HARDCODE</h3>
-            <p>HARDCODE</p>
-            <p>HARDCODE</p>
-            <p>HARDCODE</p>
         </>
         }
         
